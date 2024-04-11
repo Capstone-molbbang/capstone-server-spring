dcaf50991d558745d3b57b586dc5c4f74fb20dd9
nyeroni <nir2y@naver.com>
2024-04-11 18:40:10 +0900

    @Test 삭제

--- src/test/java/com/capstone/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/capstone/capstone/CapstoneApplicationTests.java
@@ -3,7 +3,6 @@ package com.capstone.capstone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CapstoneApplicationTests {
 
 	@Test
