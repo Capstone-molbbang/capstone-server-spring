347d92bd45b64865de3db273c6de7bcdaad474d9
nyeroni <nir2y@naver.com>
2024-05-10 00:44:10 +0900

    feat: 차 사진 아래로 내리기

--- src/main/java/com/capstone/capstone/breadRecommend/controller/BreadRecommendController.java
+++ src/main/java/com/capstone/capstone/breadRecommend/controller/BreadRecommendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BreadRecommendController {
 
-    @GetMapping("/breadPage")
+    @GetMapping("/bread")
     public String breadRecommend(){
         return "breadPage";
     }
