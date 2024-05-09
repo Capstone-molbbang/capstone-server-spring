4eb740aae891693a5cfeba8c1c413c303234b642
nyeroni <nir2y@naver.com>
2024-05-10 00:30:03 +0900

    feat: 빵 추천 페이지 생성

---
+++ src/main/java/com/capstone/capstone/breadRecommend/controller/BreadRecommendController.java
@@ -0,0 +1,13 @@
+package com.capstone.capstone.breadRecommend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BreadRecommendController {
+
+    @GetMapping("/breadPage")
+    public String breadRecommend(){
+        return "breadPage";
+    }
+}
