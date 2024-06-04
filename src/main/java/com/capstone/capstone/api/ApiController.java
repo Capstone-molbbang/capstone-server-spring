1e47ae5390648ab54c46bb17f87744d71da71ac7
nyeroni <nir2y@naver.com>
2024-06-05 01:22:54 +0900

    feat: fast api 연동 test

---
+++ src/main/java/com/capstone/capstone/api/ApiController.java
@@ -0,0 +1,35 @@
+package com.capstone.capstone.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ApiController {
+    private final RestTemplate restTemplate;
+    private static final String FASTAPI_URL = "http://3.34.126.202/test";
+    @PostMapping("/test")
+    public ResponseEntity<String> test() {
+
+        String dataToSend = "Hello from Spring Boot";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<>(dataToSend, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(FASTAPI_URL, entity, String.class);
+
+        return ResponseEntity.ok().body(response.getBody());
+    }
+
+
+}
