e891077949a9bb03db0623e5d1f0b70923258817
nyeroni <nir2y@naver.com>
2024-06-06 23:06:05 +0900

    feat: fast api 연동

--- src/main/java/com/capstone/capstone/fastapi/controller/ApiController.java
+++ src/main/java/com/capstone/capstone/fastapi/controller/ApiController.java
@@ -1,8 +1,8 @@
-package com.capstone.capstone.api;
+package com.capstone.capstone.fastapi.controller;
 
+import com.capstone.capstone.fastapi.dto.RouteRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.connector.Response;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -41,5 +41,24 @@ public class ApiController {
         System.out.println("Received data: " + data);
         data.put("processed", true);
         return ResponseEntity.ok(data);
+    }
+
+
+    @PostMapping("/api/departureTime")
+    public ResponseEntity<Map<String, Object>> sendDepartureTime(@RequestBody RouteRequest routeRequest) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("departure_time", routeRequest.getDepartureTime());
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<Map> response = restTemplate.postForEntity(FASTAPI_URL, entity, Map.class);
+
+        Map<String, Object> responseBody = response.getBody();
+
+        return ResponseEntity.ok(responseBody);
     }
 }
---
+++ src/main/java/com/capstone/capstone/fastapi/dto/RouteRequest.java
@@ -0,0 +1,11 @@
+package com.capstone.capstone.fastapi.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RouteRequest {
+
+    private LocalDateTime departureTime;
+}
