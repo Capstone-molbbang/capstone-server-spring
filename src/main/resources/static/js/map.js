e0635e992a9773c4372d1f5554e8b0c35789c74f
nyeroni <nir2y@naver.com>
2024-06-14 05:41:12 +0900

    feat: fast api 연동

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -634,7 +634,7 @@ document.addEventListener("DOMContentLoaded", function() {
         time_24hr: false,
         disableMobile: true,
         ampm: true,
-        local: "ko",
+        locale: "ko",
         onClose: function(selectedDates, dateStr, instance) {
             // 사용자가 시간을 선택한 후 실행되는 콜백 함수
             const selectedTime = instance.latestSelectedDateObj;
